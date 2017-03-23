e0219a3f84681c6efd0c5caea3399987d244c7e8
jalex7 <jalex3@gmx.de>
2017-03-23 13:28:37 +0100

    Update webapp.groovy

--- webapp.groovy
+++ webapp.groovy
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.*
  
 class Greeting {  Integer id;  String message  }
 
