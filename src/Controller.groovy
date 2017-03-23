b620511e4ef2c0d13a8ee0b30ab9ba705cf26dad
jalex7 <jalex3@gmx.de>
2017-03-23 11:08:48 +0100

    Update Controller.groovy

--- src/Controller.groovy
+++ src/Controller.groovy
@@ -1,13 +1,22 @@
 package hello
  
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
  
 @Controller
+@SpringBootApplication
 class MvcController {
-    @RequestMapping(value = "/")
+
+  @RequestMapping(value = "/")
     def @ResponseBody hello() {
         return "Hello World!"
     }
+
+    static def main(args) {
+        SpringApplication.run(Application.class, args)
+    }
+
 }
